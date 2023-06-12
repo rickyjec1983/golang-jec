90ef102c2c3627d1bace9a8168c2da111a2821be
ricky lai <ricky.lai@jec.co.id>
2023-06-12 21:21:33 +0700

    test 3

--- loop.go
+++ loop.go
@@ -4,6 +4,21 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("hello world !")
-	fmt.Println("hello world 123!")
+	//fmt.Println("hello world !")
+	//fmt.Println("hello world 123!")
+
+	for i := 1; i < 15; i++ {
+
+		if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+		if i == 14 {
+			fmt.Println("fizzbuzz")
+		}
+	}
 }
